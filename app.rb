3b76901aaef08e80c70e7491d4953d7279e2931e
erikj <erik@erikj.me>
2012-11-25 18:27:33 -0700

    adding basic /:username action and matcher

--- app.rb
+++ app.rb
@@ -14,5 +14,18 @@ Cuba.define do
       res.write render( 'views/timeline.haml', :posts=>Post.all )
     end
 
+    # /:username
+    # this should be last
+    # if user is not found, return 404
+    on "(\\w+)" do |username|
+      user = User.find_by_username(username)
+      if user
+        res.write render( 'views/user.haml', {:username=>username, :user=>user})
+      else
+        res.status = 404
+        res.write "<h1>404: Not Found</h1>"
+      end
+    end
+
   end
 end
