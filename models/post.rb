0a90006382de366df950b84b383ad999bd57bb19
erikj <erik@erikj.me>
2012-11-28 21:37:45 -0700

    converting User and Post models from RedisModel to Ohm::Model, #5

--- models/post.rb
+++ models/post.rb
@@ -1,21 +1,6 @@
-class Post < RedisModel
+class Post < Ohm::Model
 
-  property :content
-  property :user
-
-  # create a post
-  def self.create username, content
-    id = redis.incr 'nextPostId'
-    user_id = redis.get "username:#{username}:id"
-    post =  Post.new id
-
-    post.content = content
-    post.user    = user_id
-
-    user = User.new user_id
-    user.posts_push post
-    redis.rpush "global:posts", id
-    return post
-  end
+  attribute :content
+  reference :user, :User
 
 end
