b9af929a8bfb39dc05e6a60d8b6f2ab947c5448e
erikj <erik@erikj.me>
2012-11-29 21:59:22 -0700

    adding Post class method and alias for create()ing timestamped Posts, #6

--- models/post.rb
+++ models/post.rb
@@ -5,4 +5,13 @@ class Post < Ohm::Model
   attribute :created_at
   index     :created_at
 
+  class << self
+    alias_method :original_create, :create
+
+    def create options={}
+      options[:created_at] = Time.now.to_i unless options.has_key?(:created_at)
+      original_create options
+    end
+  end
+
 end
