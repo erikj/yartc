0c9066beb6a06168ed0e6efd6514f437cdfe73e9
erikj <erik@erikj.me>
2012-12-01 15:10:19 -0700

    whitespace, #7

--- models/post.rb
+++ models/post.rb
@@ -7,7 +7,7 @@ class Post < Ohm::Model
 
   def validate
     assert_numeric :user_id
-    assert_length :content, 1..140
+    assert_length  :content, 1..140
     assert_numeric :created_at
     assert_length  :created_at, 8..12
   end
