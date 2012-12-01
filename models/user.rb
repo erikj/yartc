803a65543adef41ac997d24eb8bc3422330f4cee
erikj <erik@erikj.me>
2012-12-01 15:09:45 -0700

    adding User#email attribute and validation, #7

--- models/user.rb
+++ models/user.rb
@@ -2,12 +2,13 @@ class User < Ohm::Model
 
   attribute :name
   index :name
+  attribute :email
   collection :posts, :Post
 
   def validate
     assert_present :name
     assert_length  :name, 1..64
-    # assert_email :email
+    assert_email   :email
   end
 
 end
