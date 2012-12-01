307f5f95bade20cc5f0cbcd3d2eed2f0228cce5c
erikj <erik@erikj.me>
2012-12-01 14:19:02 -0700

    adding validations on User#name, #7

--- models/user.rb
+++ models/user.rb
@@ -4,4 +4,10 @@ class User < Ohm::Model
   index :name
   collection :posts, :Post
 
+  def validate
+    assert_present :name
+    assert_length  :name, 1..64
+    # assert_email :email
+  end
+
 end
