96edd8629e713724bc12bbd3b4812f30fb3ae247
erikj <erik@erikj.me>
2012-12-08 18:43:12 -0700

    adding SHA1 salting and password hashing User-instance methods, #11

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 class User < Ohm::Model
 
   attribute :name
@@ -8,6 +10,9 @@ class User < Ohm::Model
   index :email
   unique :email
 
+  attribute :salt
+  attribute :hashed_password
+
   collection :posts, :Post
 
   def validate
@@ -15,6 +20,16 @@ class User < Ohm::Model
     assert_length  :name, 1..64
     assert_format  :name, /^\w+$/ # [a-zA-Z0-9_]
     assert_email   :email
+  end
+
+  # generate secure salt
+  def mk_salt
+    Digest::SHA1.hexdigest [Time.now.to_s, self.name, rand.to_s].join(' - ')
+  end
+
+  # generate salted password
+  def hash_password password
+    Digest::SHA1.hexdigest [self.salt, password].join(' - ')
   end
 
 end
